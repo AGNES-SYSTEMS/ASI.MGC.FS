0ede6fa0587db61c4aff54b7a408de1e3208ac2c
zeeshan4119 <mzeeshan.mehmood@hotmail.com>
2015-05-02 01:42:20 -0700

    Adding MVC5 Project

---
+++ ASI.MGC.FS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASI.MGC.FS.Startup))]
+namespace ASI.MGC.FS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
